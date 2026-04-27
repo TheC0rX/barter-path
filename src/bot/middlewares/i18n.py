ae69c96ed4dabd08fb8790887cfcc89e427d0feb
TheC0rX <therealcorx@gmail.com>
2026-04-27 23:12:07 +0300

    feat: i18n gets language from user's telegram client

--- src/bot/middlewares/i18n.py
+++ src/bot/middlewares/i18n.py
@@ -1,12 +1,28 @@
 from pathlib import Path
+
+from aiogram.types import User
+
 from aiogram_i18n import I18nMiddleware
+from aiogram_i18n.managers import BaseManager
 from aiogram_i18n.cores import FluentRuntimeCore
+
+
+class UserManager(BaseManager):
+    async def get_locale(self, event_from_user: User) -> str:
+        return (
+            event_from_user.language_code
+            if event_from_user.language_code in ["ru", "en"]
+            else "en"
+        )
+
+    async def set_locale(self, locale: str) -> None:
+        pass
 
 
 def get_i18n_middleware() -> I18nMiddleware:
     locales_path = Path(__file__).parent.parent.parent / "locales"
 
     core = FluentRuntimeCore(path=str(locales_path / "{locale}"))
-    middleware = I18nMiddleware(core=core, default_locale="en")
+    middleware = I18nMiddleware(core=core, default_locale="en", manager=UserManager())
 
     return middleware
