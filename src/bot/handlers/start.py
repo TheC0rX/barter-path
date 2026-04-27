8ba5d5bdd066f48696c9e52be34ed2b83ced45b8
TheC0rX <therealcorx@gmail.com>
2026-04-27 23:00:46 +0300

    feat: add i18n to start command

--- src/bot/handlers/start.py
+++ src/bot/handlers/start.py
@@ -1,10 +1,11 @@
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart
+from aiogram_i18n import I18nContext
 
 router = Router()
 
 
 @router.message(CommandStart())
-async def cmd_start(msg: Message) -> None:
-    await msg.answer("Hi! I'm starting...")
+async def cmd_start(message: Message, i18n: I18nContext) -> None:
+    await message.answer(i18n.get("start-welcome", name=message.from_user.first_name))  # type: ignore
