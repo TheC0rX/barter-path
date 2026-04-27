3d379ce87e1eca3adf37906f23257d97ed4965da
TheC0rX <therealcorx@gmail.com>
2026-04-27 23:02:32 +0300

    fix: parse mode change to HTML

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,5 +1,7 @@
 from loguru import logger
 from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 
 from src.bot.handlers import setup_routers
 from src.bot.middlewares.i18n import get_i18n_middleware
@@ -10,6 +12,7 @@ from src.config import config
 async def main() -> None:
     bot = Bot(
         token=config.BOT_TOKEN.get_secret_value(),
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
     )
     dp = Dispatcher()
 
