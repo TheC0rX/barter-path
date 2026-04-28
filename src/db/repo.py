7a854578dee0b4a9a58f29aff1f4a12410e27e96
TheC0rX <therealcorx@gmail.com>
2026-04-28 17:37:11 +0300

    feat: add request to db to get user using id

--- src/db/repo.py
+++ src/db/repo.py
@@ -1,4 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
 from .models import User
 
 
@@ -6,7 +8,11 @@ class UserRepo:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def add_user(self, user_id: int, locale: str):
+    async def get_user(self, user_id: int) -> User | None:
+        result = await self.session.execute(select(User).where(User.user_id == user_id))
+        return result.scalar_one_or_none()
+
+    async def add_user(self, user_id: int, locale: str) -> None:
         user = User(user_id=user_id, locale=locale)  # type: ignore
         await self.session.merge(user)
         await self.session.commit()
