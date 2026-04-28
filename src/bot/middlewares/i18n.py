c6714debdee811dc467c00095e3e0ad9db2d7d5c
TheC0rX <therealcorx@gmail.com>
2026-04-28 21:55:35 +0300

    feat: get and write language between telegram and db

--- src/bot/middlewares/i18n.py
+++ src/bot/middlewares/i18n.py
@@ -6,17 +6,29 @@ from aiogram_i18n import I18nMiddleware
 from aiogram_i18n.managers import BaseManager
 from aiogram_i18n.cores import FluentRuntimeCore
 
+from src.db.repo import UserRepo
+from src.db.base import async_session_maker
+
 
 class UserManager(BaseManager):
     async def get_locale(self, event_from_user: User) -> str:
+        async with async_session_maker() as session:
+            repo = UserRepo(session)
+            user = await repo.get_user(event_from_user.id)
+
+            if user:
+                return user.locale
+
         return (
             event_from_user.language_code
             if event_from_user.language_code in ["ru", "en"]
             else "en"
         )
 
-    async def set_locale(self, locale: str) -> None:
-        pass
+    async def set_locale(self, locale: str, event_from_user: User) -> None:
+        async with async_session_maker() as session:
+            repo = UserRepo(session)
+            await repo.add_user(user_id=event_from_user.id, locale=locale)
 
 
 def setup_i18n(dp) -> I18nMiddleware:
