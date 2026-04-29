a9d2b90f345a71011afdab00f017886fb03b91e6
TheC0rX <therealcorx@gmail.com>
2026-04-29 10:17:43 +0300

    feat: add menu keyboard to start command

--- src/bot/handlers/start.py
+++ src/bot/handlers/start.py
@@ -5,6 +5,7 @@ from aiogram_i18n import I18nContext
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.db.repo import UserRepo
+from src.bot.keyboard import inline
 
 router = Router()
 
@@ -14,4 +15,4 @@ async def cmd_start(message: Message, i18n: I18nContext, session: AsyncSession) 
     repo = UserRepo(session)
     await repo.add_user(user_id=message.from_user.id, locale=i18n.locale)  # type: ignore
 
-    await message.answer(i18n.get("start-welcome", name=message.from_user.first_name))  # type: ignore
+    await message.answer(text=i18n.get("start-welcome", name=message.from_user.first_name), reply_markup=inline.get_main_menu_kb(i18n))  # type: ignore
