bc0c5fee90caaa42c828b92d02b613258c6475ee
TheC0rX <therealcorx@gmail.com>
2026-05-16 18:27:14 +0300

    refactor(sc_api): move clients to init, change authing way

--- src/services/stalcraft_api.py
+++ src/services/stalcraft_api.py
@@ -10,43 +10,45 @@ class StalcraftAPI:
         self.owner = "EXBO-Studio"
         self.repo = "stalcraft-database"
 
-        self.api_url = "https://api.github.com"
-        self.raw_url = "https://raw.githubusercontent.com"
-
         self.headers = {
-            "Authorization": f"token {self.token}",
+            "Authorization": f"Bearer {self.token}",
             "Accept": "application/vnd.github.v3+json",
             "User-Agent": "Barter-Path",
         }
 
+        self.client = httpx.AsyncClient(
+            base_url="https://api.github.com",
+            headers=self.headers,
+            http2=True,
+        )
+        self.raw_client = httpx.AsyncClient(
+            base_url="https://raw.githubusercontent.com",
+            headers={"User-Agent": "Barter-Path"},
+            http2=True,
+        )
+
     async def get_latest_commit_sha(self) -> str:
-        url = f"{self.api_url}/repos/{self.owner}/{self.repo}/commits/main"
-        async with httpx.AsyncClient() as client:
-            response = await client.get(url, headers=self.headers)
-            response.raise_for_status()
-            return response.json()["sha"]
+        url = f"/repos/{self.owner}/{self.repo}/commits/main"
+        response = await self.client.get(url)
+        response.raise_for_status()
+        return response.json()["sha"]
 
     async def get_items_tree(self):
-        url = (
-            f"{self.api_url}/repos/{self.owner}/{self.repo}/git/trees/main?recursive=1"
-        )
-        async with httpx.AsyncClient() as client:
-            response = await client.get(url, headers=self.headers)
-            response.raise_for_status()
+        url = f"/repos/{self.owner}/{self.repo}/git/trees/main?recursive=1"
+        response = await self.client.get(url)
+        response.raise_for_status()
 
-            tree = response.json().get("tree", [])
-            return [
-                item["path"]
-                for item in tree
-                if item["path"].startswith("global/items/")
-                and item["path"].endswith(".json")
-            ]
+        tree = response.json().get("tree", [])
+        return [
+            item["path"]
+            for item in tree
+            if item["path"].startswith("global/items/")
+            and item["path"].endswith(".json")
+        ]
 
     async def fetch_json(self, path: str):
-        url = f"{self.raw_url}/{self.owner}/{self.repo}/main/{path.lstrip('/')}"
-        async with httpx.AsyncClient() as client:
-            response = await client.get(url, headers={"User-Agent": "Barter-Path"})
-            if response.status_code == 200:
-                return response.json()
-
-            return None
+        url = f"/{self.owner}/{self.repo}/main/{path.lstrip('/')}"
+        response = await self.raw_client.get(url)
+        if response.status_code == 200:
+            return response.json()
+        return None
