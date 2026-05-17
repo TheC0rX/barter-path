d3e0a9b8b07b9105c3f33debebd4fc908d9ef8f0
TheC0rX <therealcorx@gmail.com>
2026-05-17 23:41:36 +0300

    refactor: fix date format in logger

--- src/__main__.py
+++ src/__main__.py
@@ -11,13 +11,13 @@ def setup_logger() -> None:
 
     logger.add(
         RichHandler(markup=True, rich_tracebacks=True, tracebacks_show_locals=True),
-        format="{time:YYYY:MM:DD HH:mm:ss} | {level: <8} | {message}",
+        format="{time:YYYY-MM-DD HH:mm:ss} | {level: <8} | {message}",
         level="DEBUG",
     )
 
     logger.add(
         "logs/debug.log",
-        format="{time:YYYY:MM:DD HH:mm:ss} | {level: <8} | {message}",
+        format="{time:YYYY-MM-DD HH:mm:ss} | {level: <8} | {message}",
         rotation="10 MB",
         compression="zip",
         level="DEBUG",
