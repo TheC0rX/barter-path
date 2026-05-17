cee9bbf72afceba983e86cd0ca5d60a88a47fd12
TheC0rX <therealcorx@gmail.com>
2026-05-17 10:23:14 +0300

    feat: add beautiful prints by rich

--- src/__main__.py
+++ src/__main__.py
@@ -1,12 +1,23 @@
 import asyncio
+
 from loguru import logger
+from rich.logging import RichHandler
 
 from src.bot.main import main
 
 
 def setup_logger() -> None:
+    logger.remove()
+
+    logger.add(
+        RichHandler(markup=True, rich_tracebacks=True, tracebacks_show_locals=True),
+        format="{time:YYYY:MM:DD HH:mm:ss} | {level: <8} | {message}",
+        level="DEBUG",
+    )
+
     logger.add(
         "logs/debug.log",
+        format="{time:YYYY:MM:DD HH:mm:ss} | {level: <8} | {message}",
         rotation="10 MB",
         compression="zip",
         level="DEBUG",
