ea35436f88976310d2787f60b2ad14b127123bc6
TheC0rX <therealcorx@gmail.com>
2026-05-27 10:56:56 +0300

    refactor(db): change length of items id

--- src/db/models.py
+++ src/db/models.py
@@ -82,7 +82,7 @@ class TaskProgress(Base):
 class Item(Base):
     __tablename__ = "items"
 
-    id: Mapped[str] = mapped_column(VARCHAR(8), primary_key=True)
+    id: Mapped[str] = mapped_column(VARCHAR(5), primary_key=True)
     category: Mapped[str] = mapped_column(VARCHAR(64), index=True)
     name_ru: Mapped[str] = mapped_column(VARCHAR(128))
     name_en: Mapped[str] = mapped_column(VARCHAR(128))
