0f94965e8a684c050641cf20eed4556417b55126
TheC0rX <therealcorx@gmail.com>
2026-06-03 21:53:12 +0300

    refactor(db): change add user function

--- src/db/repo.py
+++ src/db/repo.py
@@ -13,17 +13,11 @@ class UserRepo:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def get_user(self, user_id: int) -> User | None:
-        result = await self.session.execute(select(User).where(User.user_id == user_id))
-        return result.scalar_one_or_none()
-
     async def add_user(self, user_id: int, locale: str) -> None:
         stmt = (
             insert(User)
             .values(user_id=user_id, locale=locale)
-            .on_conflict_do_update(
-                index_elements=[User.user_id], set_=dict(locale=locale)
-            )
+            .on_conflict_do_nothing(index_elements=[User.user_id])
         )
         await self.session.execute(stmt)
         await self.session.commit()
