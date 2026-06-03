656bf339ca85f331aebb827887a518c478bd6ec5
TheC0rX <therealcorx@gmail.com>
2026-06-03 21:50:23 +0300

    refactor: remove user registration in start command

--- src/bot/handlers/start.py
+++ src/bot/handlers/start.py
@@ -4,7 +4,6 @@ from aiogram.filters import CommandStart
 from aiogram_i18n import I18nContext
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.db.repo import UserRepo
 from src.bot.utils.ui import show_main_menu
 
 router = Router()
@@ -12,7 +11,4 @@ router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message, i18n: I18nContext, session: AsyncSession) -> None:
-    repo = UserRepo(session)
-    await repo.add_user(user_id=message.from_user.id, locale=i18n.locale)  # type: ignore
-
     await show_main_menu(message, session, i18n)
